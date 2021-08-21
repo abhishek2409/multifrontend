9ae72801697e0eaf5e0adfc395b81288fa329a52
Abhishek Wadhawan <abhishek.wadhawan@ymail.com>
2021-08-21 18:58:41 +0530

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
     return (
         <div>
-            Container App
+            Container App V2
             <MarketingApp />
         </div>
     )
